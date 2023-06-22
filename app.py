3003edc05cef6387b7013c341e2275e61c4ea2e9
Sergio A <powerserg79@users.noreply.github.com>
2023-06-22 09:36:35 +0100

    update app to use_async explicitly

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import chainlit as cl
 
 import os
-huggingfacehub_api_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
+
+huggingfacehub_api_token = os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
 from langchain import HuggingFaceHub, PromptTemplate, LLMChain
 
 repo_id = "tiiuae/falcon-7b-instruct"
-llm = HuggingFaceHub(huggingfacehub_api_token=huggingfacehub_api_token, 
-                     repo_id=repo_id, 
-                     model_kwargs={"temperature":0.6, "max_new_tokens":2000})
+llm = HuggingFaceHub(
+    huggingfacehub_api_token=huggingfacehub_api_token,
+    repo_id=repo_id,
+    model_kwargs={"temperature": 0.6, "max_new_tokens": 2000},
+)
 
 template = """
 You are an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
@@ -18,7 +21,7 @@ You are an artificial intelligence assistant. The assistant gives helpful, detai
 """
 
 
-@cl.langchain_factory
+@cl.langchain_factory(use_async=False)
 def factory():
     prompt = PromptTemplate(template=template, input_variables=["question"])
     llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
